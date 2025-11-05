199dcc2fd354d1b3503dd7fe371bb4e749367834
Rafael Takahashi <rafael.takahashi20@outlook.com>
2025-11-05 10:45:33 -0300

    feat: use pprint

--- src/main.py
+++ src/main.py
@@ -1,16 +1,17 @@
 import sys
 from src.components.parser import Parser
 from src.utils.read_file import file_to_buffer
+import pprint
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python -m src.main <input_path>")
-        sys.exit(1)
+  if len(sys.argv) < 2:
+    print("Usage: python -m src.main <input_path>")
+    sys.exit(1)
 
-    code_path = sys.argv[1]
-    buffer: str = file_to_buffer(path=code_path)
-    parser = Parser()
-    parser.parse(buffer)
+  code_path = sys.argv[1]
+  buffer: str = file_to_buffer(path=code_path)
+  parser = Parser()
+  pprint.pprint(parser.parse(buffer))
 
 if __name__ == "__main__":
-    main()
+  main()
